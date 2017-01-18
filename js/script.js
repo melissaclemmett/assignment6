642282b8239e48008b62b4693b34c5357ef1d6c3
Melissa Clemmett <MClemmett@ISPTNET.LOCAL>
2017-01-18 21:01:57 +1100

    Committing final version of assignment

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 $(document).ready(start); //Delay code from executing until all DOM assets have been loaded
 
-var cityList = ["NYC", "SF", "LA", "ATX", "SYD"]; //Global variable
+var cityList = ["NYC", "SF", "LA", "ATX", "SYD"]; //Global variable - array of cities
 
 function start() {
   console.log("Ready"); //Testing function in console
 
   $(cityList).each(function(index, element){
-    $("#city-type").append("<option value='" + element + "'>" + element + "</option>");
+    $("#city-type").append("<option value='" + element + "'>" + element + "</option>"); //Insert values to drop-down menu from cityList array
   });
 
-  // $("form").on("click", "#city-type", function() {
-  //   $(this).find("option").eq(0).html("");
-  // });
+  $("label").on("click", "#city-type", function() { //When user clicks on form, clear option 0
+    $(this).find("option").eq(0).html("");
+  });
 
-  $("form").change(function(e) { //Prevent a form submission
-    e.preventDefault();
+  $("label").change(function(e) { //When user changes input...
+    e.preventDefault(); //Prevent a form submission
     console.log("Form submission prevented"); //Testing function in console
 
-    displayCity(); //Display city when user clicks "submit" button
+    displayCity(); //Display city
     // alert("TEST");
   });
 }
 
 function displayCity () {
-  var city = $("#city-type").val(); //Get string value of user's unput
+  var city = $("#city-type").val(); //Get value of user input
 
-  if(city == cityList[0]) { //If user submits "new york" or "new york city" or "nyc"
+  if(city == cityList[0]) { //If user inputs "NYC"
     $("body").attr("class", "nyc"); // Get first element from attribute name - to make background of page nyc.jpg
   }
-  else if(city == cityList[1]) { //Else if user submits "san francisco" or "sf" or "bay area"
+  else if(city == cityList[1]) { //Else if inputs "SF"
     $("body").attr("class", "sf"); // Get first element from attribute name - to make background of page sf.jpg
   }
   else if(city == cityList[2]) {
